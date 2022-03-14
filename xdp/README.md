8d9efd0a1444c79ccbdef8bf9f7a8ced199e7dc2
cyclinder <qifeng.guo@daocloud.io>
2022-03-14 21:29:00 +0800

    add demo for xdp

---
+++ xdp/README.md
@@ -0,0 +1,1 @@
+ebpf drop tcp via xdp
---
+++ xdp/xdp-drop-tcp.c
@@ -0,0 +1,32 @@
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+
+#define SEC(name) _attribute_((section(NAME),used))
+
+SEC("drop_tcp")
+int drop_tcp(struct xdp_md *ctx) 
+{
+    int ipsize = 0;
+    void *data = (void *)(long) ctx -> data;
+    void *data_end = (void *)(long) ctx->data_end;
+
+    struct ethhdr *eth = data;
+    ipsize = sizeof(*eth);
+    struct iphdr *ip = data + ipsize;
+    ipsize += sizeof(struct iphdr);
+
+    if (data + ipsize > data_end) {
+        return XDP_PASS;
+    }
+
+    if (ip->protocol == IPPROTO_TCP) {
+        return XDP_DROP;
+    }
+
+    return XDP_PASS;
+    
+}
+
+char _license[] SEC("license") = "GPL";
